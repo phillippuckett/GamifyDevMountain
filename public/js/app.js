936920b73195c50cab07f8b01b65d5c7095183c3
Phillip Puckett <phillippuckett88@gmail.com>
2016-02-29 19:14:10 -0700

    Student Experience Almost Completed

--- public/js/app.js
+++ public/js/app.js
@@ -16,42 +16,19 @@ angular.module("GamifyDevMountain", ['ui.router'])
                 controller: 'authCtrl'
             })
 
-        /** User State */
-            .state('user', {
-                url: '/user',
-                templateUrl: 'html/states/userState.html',
-                controller: 'userCtrl'
+        /** Student State */
+            .state('student', {
+                url: '/student',
+                templateUrl: 'html/states/studentState.html',
+                controller: 'studentCtrl'
             })
 
         /** Admin State */
             .state('adminState', {
-                name: 'admin',
                 url: '/admin',
                 templateUrl: './html/states/adminView.html',
                 controller: 'adminCtrl'
             })
 
-            .state('studentState', {
-                name: 'student',
-                url: '/student',
-                templateUrl: './html/states/studentView.html',
-                controller: 'studentCtrl'
-            })
-
         $urlRouterProvider.otherwise('/login');
     });
-
-
-
-// resolve: {
-//     user: function (authSvc, $state) {
-//         return authSvc.getCurrentUserObject().then(function (response) {
-//             if (response.status != 200) {
-//                 $state.go('login')
-//             }
-//             console.log(response);
-//             return response.data;
-//         })
-//         console.log('Resolve in login');
-//     }
-// }
