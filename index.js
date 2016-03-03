67b0d2c5d0cdf9d3ad05f8898f2dce704a0bd249
Phillip Puckett <phillippuckett88@gmail.com>
2016-03-03 11:38:47 -0700

    Minor Change in index.js

--- index.js
+++ index.js
@@ -75,8 +75,9 @@ app.get('/api/currentUser', usersCtrl.getCurrentUser);
 app.get('/api/users', usersCtrl.getUser);
 app.put('/api/users/:id', usersCtrl.updateUser);
 app.delete('/api/users/:id', usersCtrl.deleteUser);
+
 app.put('/api/users/badgerequest/:badge', usersCtrl.requestBadge);
-// app.get('api/users/badgesawarded/:badge', usersCtrl.requestBadge);
+// app.get('api/users/badgesawarded/:badge', usersCtrl.awardedBadge);
 app.get('/api/getUserInfo', usersCtrl.getInformation);
 app.get('/api/getStudentsByCohort/:cohort', usersCtrl.getStudentsByCohort);
 app.put('/api/badgeApproval', usersCtrl.badgeApproval);
