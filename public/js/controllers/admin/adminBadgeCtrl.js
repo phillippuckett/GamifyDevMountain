4c654b40e15ecb67e6392c63b72e8923e3ee53d6
Tyson Taylor <mellowt@gmail.com>
2016-03-06 23:52:54 -0700

    added an update and a delete button to badgesAdmin, working on adding a confirmation modal for deletion.

--- public/js/controllers/admin/adminBadgeCtrl.js
+++ public/js/controllers/admin/adminBadgeCtrl.js
@@ -1,18 +1,25 @@
-angular.module("GamifyDevMountain").controller("adminBadgeCtrl", ['$scope', 'adminStateSvc', function($scope, adminStateSvc) {
+angular.module( "GamifyDevMountain" )
+  .controller( "adminBadgeCtrl", [ '$scope', 'adminStateSvc', function ( $scope, adminStateSvc ) {
 
-  var aSvc = adminStateSvc;
+    var ass = adminStateSvc;
 
-      // ############### Badges ################ \\
-      $scope.badges = [];
-      console.log('adminBadgeCtrl is logging');
-      aSvc.getBadges()
+    // ############### Badges ################ \\
+    $scope.badges = [];
+    console.log( 'adminBadgeCtrl is logging' );
+    ass.getBadges()
+      .then( function ( response ) {
+        console.log( 'badges', response );
+        $scope.badges = response;
+      } );
+
+    $scope.updateBadge = function ( id, updatedBadge ) {
+      $scope.getBadges( id, updateBadge )
         .then( function ( response ) {
-          console.log('badges', response);
-          $scope.badges = response;
+          $scope.getBadges();
         } );
+    }
 
-
-}]);
+} ] );
 
 
 
---
+++ public/js/controllers/admin/confDelMoAdmin.js
@@ -0,0 +1,3 @@
+angular.module("GamifyDevMountain").controller("confDelMoAdminCtrl", [ '$scope', '$location', 'close', 'categorySvc', 'adminStateSvc', function ( $scope, $location, close, categorySvc, adminStateSvc ) {
+  
+});
